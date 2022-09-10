6666106b61eb6bdb437d4f0862e78520b1cd6c31
tom <tomphill@live.co.uk>
2022-09-10 10:04:11 +0800

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  images: {
+    domains: [],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./utils/**/*.js",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: ["Aboreto", "cursive"],
+        body: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
