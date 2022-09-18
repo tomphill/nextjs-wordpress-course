af9abe792575168b09a640095d8f439d7ebb3112
tom <tomphill@live.co.uk>
2022-09-19 07:52:41 +0800

    fix page generation

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -27,11 +27,13 @@ export const getStaticPaths = async () => {
   });
 
   return {
-    paths: [...data.pages.nodes, ...data.properties.nodes].map((page) => ({
-      params: {
-        slug: page.uri.substring(1, page.uri.length - 1).split("/"),
-      },
-    })),
+    paths: [...data.pages.nodes, ...data.properties.nodes]
+      .filter((page) => page.uri === "/")
+      .map((page) => ({
+        params: {
+          slug: page.uri.substring(1, page.uri.length - 1).split("/"),
+        },
+      })),
     fallback: false,
   };
 };
