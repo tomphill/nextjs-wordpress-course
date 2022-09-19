eb71b70f2970667af4f6b2fe7690293bf696fd54
tom <tomphill@live.co.uk>
2022-09-19 08:00:10 +0800

    page generation fix

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -28,7 +28,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths: [...data.pages.nodes, ...data.properties.nodes]
-      .filter((page) => page.uri === "/")
+      .filter((page) => page.uri !== "/")
       .map((page) => ({
         params: {
           slug: page.uri.substring(1, page.uri.length - 1).split("/"),
